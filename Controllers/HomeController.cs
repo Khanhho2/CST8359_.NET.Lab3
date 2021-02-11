29062e24f6b82f14826fdbfecdc2d0492270536d
Nguyen Gia Khanh Ho <ho000089@algonquinlive.com>
2021-02-11 11:46:31 -0500

    Lab 3

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Lab3.Models;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Razor()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Count()
+        {
+            HttpContext.Session.SetString("bottles", Request.Form["bottles"]);
+
+            return View();
+        }
+
+        public IActionResult CreatePerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+        [Required]
+        [StringLength(255)]
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(255)]
+        public string LastName
+        {
+            get;
+            set;
+        }
+        [Required]
+        [Range(1, 200)]
+        public string Age
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+        [Required]
+        [DataType(DataType.Date)]
+        public string Birthday
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(100)]
+        [BindProperty(Name = "pass")]
+        public string Password
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(255)]
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
